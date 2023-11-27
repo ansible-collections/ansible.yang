c309fa281302a2eba4163d7b3d1b505df05aba78
KB-perByte <sagpaul@redhat.com>
2023-11-27 14:51:39 +0530

    fix json2xml

--- plugins/lookup/json2xml.py
+++ plugins/lookup/json2xml.py
@@ -71,13 +71,13 @@ _raw:
     description: The translated xml string from json
 """
 
+import importlib
 import json
 import os
 
 from ansible.errors import AnsibleLookupError
-
-# from ansible.module_utils.six import raise_from
 from ansible.module_utils._text import to_text
+from ansible.module_utils.six import raise_from
 from ansible.plugins.lookup import LookupBase
 from ansible.utils.display import Display
 
@@ -85,12 +85,7 @@ from ansible_collections.ansible.yang.plugins.common.base import JSON2XML_DIR_PA
 from ansible_collections.ansible.yang.plugins.module_utils.translator import Translator
 
 
-# try:
-#     import pyang  # noqa
-# except ImportError as imp_exc:
-#     PYANG_IMPORT_ERROR = imp_exc
-# else:
-#     PYANG_IMPORT_ERROR = None
+PYANG_IMPORT_ERROR = None
 
 
 display = Display()
@@ -107,11 +102,18 @@ class LookupModule(LookupBase):
         display.vvvv(msg)
 
     def run(self, terms, variables, **kwargs):
-        # if PYANG_IMPORT_ERROR:
-        #     raise_from(
-        #         AnsibleLookupError("pyang must be installed to use this plugin"),
-        #         PYANG_IMPORT_ERROR,
-        #     )
+        global PYANG_IMPORT_ERROR
+        for module in ["pyang"]:
+            try:
+                importlib.import_module(module)
+            except ImportError as imp_exc:
+                PYANG_IMPORT_ERROR = imp_exc
+
+        if PYANG_IMPORT_ERROR:
+            raise_from(
+                AnsibleLookupError("pyang must be installed to use this plugin"),
+                PYANG_IMPORT_ERROR,
+            )
 
         res = []
         try:
